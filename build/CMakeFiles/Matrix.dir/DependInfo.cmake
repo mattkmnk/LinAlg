b4f0a2db676696372b03cb8773b9b1078b214cf3
Mateusz Kaminski <fressekk@gmail.com>
2022-07-14 17:30:12 +0200

    CMake configuration

---
+++ build/CMakeFiles/Matrix.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/mateu/Desktop/Learning/LinAlg/LinAlg/Matrix.cpp" "CMakeFiles/Matrix.dir/LinAlg/Matrix.cpp.obj" "gcc" "CMakeFiles/Matrix.dir/LinAlg/Matrix.cpp.obj.d"
+  "C:/Users/mateu/Desktop/Learning/LinAlg/LinAlg/Row.cpp" "CMakeFiles/Matrix.dir/LinAlg/Row.cpp.obj" "gcc" "CMakeFiles/Matrix.dir/LinAlg/Row.cpp.obj.d"
+  "C:/Users/mateu/Desktop/Learning/LinAlg/main.cpp" "CMakeFiles/Matrix.dir/main.cpp.obj" "gcc" "CMakeFiles/Matrix.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Matrix.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Matrix.dir/LinAlg/Matrix.cpp.obj"
+  "CMakeFiles/Matrix.dir/LinAlg/Matrix.cpp.obj.d"
+  "CMakeFiles/Matrix.dir/LinAlg/Row.cpp.obj"
+  "CMakeFiles/Matrix.dir/LinAlg/Row.cpp.obj.d"
+  "CMakeFiles/Matrix.dir/main.cpp.obj"
+  "CMakeFiles/Matrix.dir/main.cpp.obj.d"
+  "Matrix.exe"
+  "Matrix.exe.manifest"
+  "Matrix.pdb"
+  "libMatrix.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Matrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
